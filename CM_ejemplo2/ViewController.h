e6b3a97e00172b0297a5d69c1cbec790fe3ed999
Vita V <dj.vita.09@gmail.com>
2015-06-09 15:38:21 -0500

    Initial Commit

---
+++ CM_ejemplo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CM_ejemplo2
+//
+//  Created by vita on 6/9/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CM_ejemplo2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CM_ejemplo2
+//
+//  Created by vita on 6/9/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
